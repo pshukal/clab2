c9053c57a4b71bdea22095e416406bdb49e5f7af
Punit Shukal <pshukal@asu.edu>
2025-04-04 18:09:26 -0700

    Made new files

---
+++ VisitorDesignPattern/VisitorDesignPattern/src/AltVehicleInspection.java
@@ -0,0 +1,49 @@
+public class AltVehicleInspection implements IVehicleInspector {
+    @Override
+    public int visit(Car car) {
+        int serviceCharge = 0;
+        if(car.getColor()=="Black"){
+            serviceCharge += 100;
+        }else{
+            serviceCharge += 50;
+        }
+        System.out.println("Service Charge for Car: " + serviceCharge);
+        return serviceCharge;
+    }
+
+    @Override
+    public int visit(Van van) {
+        int serviceCharge = 0;
+        if(van.getNumberOfDoors()>4){
+            serviceCharge += 500;
+        }else{
+            serviceCharge += 100;
+        }
+        System.out.println("Service Charge for Van: " + serviceCharge);
+        return serviceCharge;
+    }
+
+    @Override
+    public int visit(Motorbike motorbike) {
+        int serviceCharge = 0;
+        if(motorbike.getEngineCapacity()>=200){
+            serviceCharge += 200;
+        }else{
+            serviceCharge += 50;
+        }
+        System.out.println("Service Charge for Motorbike: " + serviceCharge);
+        return serviceCharge;
+    }
+
+    public class VehicleFactory {
+        private VehicleFactory() {} // private constructor
+
+        public static IVehicleInspector getVehicleInspector(String prop) {
+            if ("alt".equalsIgnoreCase(prop)) {
+                return new AltVehicleInspection();
+            }
+            return new VehicleInspection();
+        }
+    }
+
+}
